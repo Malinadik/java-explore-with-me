c92c0976540393b3a5ac6d67d7b6409e7b72d5d5
Marina <copperraspberry@yandex.ru>
2023-07-21 00:43:45 +0300

    feat: добавлен сервис статистики

--- stats-service/client/src/main/ru.practicum/java/client/StatClient.java
+++ stats-service/client/src/main/ru.practicum/java/client/StatClient.java
@@ -1,5 +1,6 @@
 package client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
