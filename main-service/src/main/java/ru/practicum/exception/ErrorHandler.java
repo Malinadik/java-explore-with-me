8747753b384147cff23b68e18d70f60361692091
Marina <copperraspberry@yandex.ru>
2023-08-01 23:56:32 +0300

    fix: отклавливаются все необходимые ошибки

--- main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -6,8 +6,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ValidationException;
+
 @RestControllerAdvice
 public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validationException(final ValidationException e) {
+        return new ErrorResponse("Available error", e.getMessage());
+    }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
@@ -17,7 +25,7 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse validationException(final MethodArgumentNotValidException e) {
-        return new ErrorResponse("Validation error", e.getMessage());
+        return new ErrorResponse("MethodArgumentNotValidException", e.getMessage());
     }
 
     @ExceptionHandler
@@ -36,5 +44,17 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse conflictException(final ConflictException e) {
         return new ErrorResponse("Conflict error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validationException(final NotSupportedStateException e) {
+        return new ErrorResponse("NotSupportedStateException", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse allException(final Exception e) {
+        return new ErrorResponse("Exception", e.getMessage());
     }
 }
